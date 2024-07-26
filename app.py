3126eda58c24022ffd6b2257633888acd468f5a3
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-07-26 01:25:47 -0300

    curso concluido e todas as funcoes finalizadas

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ if response.status_code == 200:
                 "item": item['Item'],
                 "price": item['price'],
                 "description": item['description']
-            })
+            }) #adiciona os nomes dos restaurantes e seus dados
 
 else:
     print(f'O erro foi {response.status_code}')
 
-for nome_do_restaurante, dados in dados_restaurante.items():
+for nome_do_restaurante, dados in dados_restaurante.items(): #criando cada restaurnate em seu respectivo nome e criando arquivos json com eles
     nome_do_arquivo = f'{nome_do_restaurante}.json'
     with open(nome_do_arquivo, 'w') as arquivo_restaurante:
         json.dump(dados, arquivo_restaurante, indent=4)
